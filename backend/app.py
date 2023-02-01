6579fbd99e37b2c3ec94e72e810a1261e55c6e84
Trinity <trinitysazo@gmail.com>
2023-01-31 19:02:24 -0500

    Added backend

---
+++ backend/app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/profile')
+def my_profile():
+    response_body = {
+        "name": "Nagato",
+        "about": "Hello! I'm a full stack developer that loves python and javascript"
+    }
+
+    return response_body
+
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
