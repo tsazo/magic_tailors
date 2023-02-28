9279d9bfc86a6232b037c5dbf7f3306d7c13a34e
Trinity <trinitysazo@gmail.com>
2023-02-28 15:24:30 -0500

    Design updates

--- src/components/home/HomeHeader.js
+++ src/components/home/HomeHeader.js
@@ -14,20 +14,34 @@ const HomeHeader = () => {
   const [randomData, setRandomData] = useState(() => getRandomObject(slogans));
 
   return (
-    <div className='home-header'>
+    // <div style="height: 100vh;">
+    //   <div class="home-header">
+    //     <div class="home-header-text">
+    //       <h1>MAGIC TAILOR'S</h1>
+    //       <h3>FASHION MAGIC AT WORK</h3>
+    //     </div>
+    //     <a class="button" href="/book" style="background-color: rgb(43, 80, 170);">SCHEDULE APPOINTMENT</a>
+    //   </div>
+    //   <div style="background-color: black; height: -webkit-fill-available;">
+    //   </div>
+    // </div>
+    <div className='home-header-container'>
+      <div className='home-header'>
 
-        <div className='home-header-text'>
-            {/* <Heading fontSize='50px'>MAGIC TAILOR'S</Heading> */}
-            <h1>MAGIC TAILOR'S</h1>
-            {/* <p style={{fontSize: '18px'}}><b>{randomData}</b></p> */}
-            <h3>{randomData}</h3>
-        </div>
-
-
-        <Button to="/book">
-          SCHEDULE APPOINTMENT
-        </Button>
+      <div className='home-header-text'>
+          {/* <Heading fontSize='50px'>MAGIC TAILOR'S</Heading> */}
+          <h1>MAGIC TAILOR'S</h1>
+          {/* <p style={{fontSize: '18px'}}><b>{randomData}</b></p> */}
+          <h3>{randomData}</h3>
       </div>
+
+
+      <Button to="/book">
+        SCHEDULE APPOINTMENT
+      </Button>
+      </div>
+
+    </div>
   );
 }
 
